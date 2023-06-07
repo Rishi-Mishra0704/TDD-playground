f3bed09c5810a8393127e597e20f6497c0ceca11
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-06-07 15:05:39 +0530

    Fix linter errors

--- spec/Solver_spec.rb
+++ spec/Solver_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Solver do
       expect(solver.factorial(0)).to eq(1)
     end
 
-    it 'returns the correct factorial for positive integers' do
+    it 'returns the correct factorial for positive integers.' do
       solver = Solver.new
       expect(solver.factorial(5)).to eq(120)
       expect(solver.factorial(6)).to eq(720)
